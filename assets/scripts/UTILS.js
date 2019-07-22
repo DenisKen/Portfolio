62807bbd59b266e386778cd26456b7003ef65f22
DenisKen <denis.kenno@gmail.com>
2019-07-22 16:46:09 -0300

    Creating a React project

--- assets/scripts/UTILS.js
+++
@@ -1,9 +0,0 @@
-var UTILS = {
-
-	elements:{
-		canvas3D: document.getElementById("canvas3D"),
-		aboutMe:document.getElementById("AboutMe"),
-		aboutMe_Arrow: document.getElementById("aboutMe_element_1"),
-		aboutMe_Title: document.getElementById("aboutMe_element_2")
-	}
-}
