145d453dbce08d373e9215dd0a73d3b675a1fbf8
DenisKen <denis.kenno@gmail.com>
2019-07-22 18:13:01 -0300

    Trying put three js in project

---
+++ my-portfolio/src/Three/SceneManager.js
@@ -0,0 +1,48 @@
+//First of all we need to create the React component that will host the canvas and the Three.js application.
+import React, {Component} from 'react';
+import * as THREE from 'three';
+import * as FBXLoader from 'three-fbx-loader';
+
+class SceneManager extends Component{
+
+    componentDidMount() {
+        const scene = new THREE.Scene();
+        const camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+        const renderer = new THREE.WebGLRenderer();
+        renderer.setSize( window.innerWidth, window.innerHeight );
+        // document.body.appendChild( renderer.domElement );
+        // use ref as a mount point of the Three.js scene instead of the document.body
+        this.mount.appendChild( renderer.domElement );
+        var light = new THREE.AmbientLight( 0x404040 ); // soft white light
+        scene.add( light );
+        var loader = new FBXLoader();
+        console.log(loader);
+        var texture = new THREE.TextureLoader().load("./Three/Models/Ali/diffuse_01.png");
+        loader.load('./Three/Models/Ali/Ali.fbx', function (object3d) {
+            object3d.traverse(function (child) {
+                if (child instanceof THREE.Mesh) {
+            
+                    // apply texture
+                    child.material.map = texture
+                    child.material.needsUpdate = true;
+                }
+            });
+            scene.add(object3d);
+            console.log("Hi");
+        });
+       
+        camera.position.z = 0;
+        var animate = function () {
+          requestAnimationFrame( animate );
+          renderer.render( scene, camera );
+        };
+        animate();
+      }
+      render() {
+        return (
+          <div ref={ref => (this.mount = ref)} />
+        )
+      }
+}
+
+export default SceneManager;
