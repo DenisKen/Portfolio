ce13abdb123bac3cbf24b7cfaa3333a722bbe70b
DenisKen <denis.kenno@gmail.com>
2019-08-13 11:27:14 -0300

    Update

--- my-portfolio/src/MySkills/MySkills.js
+++ my-portfolio/src/MySkills/MySkills.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import './MySkills.css';
 
-import icon_react from './Icons/Icon-react.png';
+import icon_react from 'simple-icons/icons/react';
+
+import { ReactComponent as Icon_Javascript} from 'simple-icons/icons/javascript.svg';
+import { ReactComponent as Icon_HTML} from 'simple-icons/icons/html5.svg';
+import { ReactComponent as Icon_CSS3} from 'simple-icons/icons/css3.svg';
+import { ReactComponent as Icon_React} from 'simple-icons/icons/react.svg';
+import { ReactComponent as Icon_Unity} from 'simple-icons/icons/unity.svg';
+import { ReactComponent as Icon_Three} from 'simple-icons/icons/trakt.svg';
+
 
 const mySkills = () => {
 
-
+    const x = require('simple-icons/icons/react');
+    console.log(x.svg);
     return (
         <div className="Global-Background">
             <div className="Global-Content">
@@ -19,24 +28,28 @@ const mySkills = () => {
                     </div>
                     <div className="MySkills-Skills">
                         <ul>
-                            <li>
-                                <img src={icon_react}/>
+                            <li>   
+                                <Icon_Javascript className="MySkills-Skills--Icon"/>
                                 <p>Javascript</p>
                             </li>
                             <li>
-                                <img src={icon_react}/>
-                                <p>HTML/CSS</p>
+                                <Icon_HTML className="MySkills-Skills--Icon"/>
+                                <p>HTML</p>
                             </li>
                             <li>
-                                <img src={icon_react}/>
+                                <Icon_CSS3 className="MySkills-Skills--Icon"/>
+                                <p>CSS</p>
+                            </li>
+                            <li>
+                                <Icon_React className="MySkills-Skills--Icon"/>
                                 <p>React</p>
                             </li>
                             <li>
-                                <img src={icon_react}/>
+                                <Icon_Unity className="MySkills-Skills--Icon"/>
                                 <p>Unity3D</p>
                             </li>
                             <li>
-                                <img src={icon_react}/>
+                                <Icon_Three className="MySkills-Skills--Icon"/>
                                 <p>ThreeJS</p>
                             </li>
                         </ul>
