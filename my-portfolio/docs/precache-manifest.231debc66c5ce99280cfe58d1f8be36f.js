4ea67ae060b37d09690bd90290dd241af99a3c95
DenisKen <denis.kenno@gmail.com>
2019-08-13 11:54:12 -0300

    Update

---
+++ my-portfolio/docs/precache-manifest.231debc66c5ce99280cfe58d1f8be36f.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7870e17b6bda8233d0cb1d429e1f48b3",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "5f76c6f74d99a120f2ed",
+    "url": "/portfolio/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "20bf53f1f7782f372bd2",
+    "url": "/portfolio/static/css/main.1cba9207.chunk.css"
+  },
+  {
+    "revision": "5f76c6f74d99a120f2ed",
+    "url": "/portfolio/static/js/2.a5c4d959.chunk.js"
+  },
+  {
+    "revision": "20bf53f1f7782f372bd2",
+    "url": "/portfolio/static/js/main.785a7989.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "ca21bfd926a070617e9f16c3344da30d",
+    "url": "/portfolio/static/media/Giphy.ca21bfd9.gif"
+  },
+  {
+    "revision": "56242f4e93410d941e83b4fc4cdd2b23",
+    "url": "/portfolio/static/media/Main.56242f4e.jpg"
+  },
+  {
+    "revision": "b675af6016eed403de51899be75efe59",
+    "url": "/portfolio/static/media/css3.b675af60.svg"
+  },
+  {
+    "revision": "3056bf2f96c40cd0a814b355959a6983",
+    "url": "/portfolio/static/media/html5.3056bf2f.svg"
+  },
+  {
+    "revision": "1f6069572c9380a733700436d2980f8a",
+    "url": "/portfolio/static/media/javascript.1f606957.svg"
+  },
+  {
+    "revision": "092e0882358cd09dbe9376b64d0f2a0e",
+    "url": "/portfolio/static/media/react.092e0882.svg"
+  },
+  {
+    "revision": "617df478106df4bfdf8b2f43aead3ab1",
+    "url": "/portfolio/static/media/trakt.617df478.svg"
+  },
+  {
+    "revision": "5a105c2d2547def29d7a51151c9379db",
+    "url": "/portfolio/static/media/unity.5a105c2d.svg"
+  }
+]);
