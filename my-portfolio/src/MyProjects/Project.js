eea7dfceec42e3f5587ded5dd706b04e8812f402
DenisKen <denis.kenno@gmail.com>
2019-08-15 00:23:06 -0300

    Update

--- my-portfolio/src/MyProjects/Project.js
+++ my-portfolio/src/MyProjects/Project.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import './Project.css'
 
+import video from './Videos/Tiki.mp4';
 
 const project = (props) => {
-
+    
     return (
         <div className="Project-Content" style={{marginTop: props.marginTop}}>
             <div className="Project-Picture">
                 <div>
                     <div className="Project-Picture--Active">
+                        <video loop autoPlay>
+                            <source src={video} type="video/mp4"/>
+                        </video>
                         <img src={props.gif} alt={props.alt}/>
                     </div>
                     <img className="Project-Picture--Static" src={props.image} alt={props.alt}/>
