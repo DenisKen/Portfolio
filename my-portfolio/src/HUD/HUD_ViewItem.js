713ee62f51e446a1eb25e0f0429124ef77284214
DenisKen <denis.kenno@gmail.com>
2019-09-02 09:51:05 -0300

    Update

--- my-portfolio/src/HUD/HUD_ViewItem.js
+++ my-portfolio/src/HUD/HUD_ViewItem.js
@@ -8,13 +8,83 @@ import photo3 from './Images/3.png';
 import arrom_left from './Images/Arrow_left.png';
 import arrom_right from './Images/Arrow_right.png';
 
-class HUD_Html extends Component {
+class HUD_ViewItem extends Component {
 
-    style = {
-        width: '100%',
-        height: '100%'
+    leftArrow = {
+        visibility: 'hidden'
+    }
+    rightArrow = {
+        visibility: 'hidden'
     }
 
+    constructor(props){
+        super(props);
+
+        this.state = {
+            image: null
+        }
+
+        this.images = {
+            photo1: photo1,
+            photo2: photo2
+        }
+
+        this.imagesToShow = [];
+        this.index = 0;
+    }
+    
+    enableViewItem(imagesNames){
+        //here we enable PointerEvents and SetVisible the HUD
+
+        //We get images names and put in a array to show them.
+        for (let i = 0; i < imagesNames.length; i++) {
+            this.imagesToShow.push(this.images[imagesNames[i]]);
+        }
+        //Set first image and show it.
+        this.setState({
+            image: this.images[this.imagesToShow[0]]
+        })
+
+        //VerifyArrows
+        if (this.hasPreviousImage())
+            this.enableArrowLeft();
+        
+        if (this.hasNextImage())
+            this.enableArrowRight();
+        
+        
+    }
+    disableViewItem(){
+
+    }
+
+    hasPreviousImage(){
+        
+    }
+  
+    hasNextImage(){
+        if (this.index == this.imagesToShow.length)
+            return true;
+        return false;
+    }
+
+    clickArrowLeft(){
+        
+        this.index -= 1;
+
+    }
+    clickArrowRight(){
+
+        this.index += 1;
+        //Change image (index and set)
+    }
+
+    enableArrowLeft(){
+        
+    }
+    enableArrowRight(){
+
+    }
     
     render(){
 
@@ -27,8 +97,8 @@ class HUD_Html extends Component {
                 </div>;
 
         const landscape = <div className="HUD_Html-ViewItem--Content HUD_Html-ViewItem--Landscape">
-                    <img className="HUD_Html-ViewItem--Arrow HUD_Html-ViewItem--Arrow-Left" src={arrom_left}/>
-                    <img className="HUD_Html-ViewItem--MainImage" src={photo1}/>
+                    <input className="HUD_Html-ViewItem--Arrow HUD_Html-ViewItem--Arrow-Left" type="image" src={arrom_left}/>
+                    <img className="HUD_Html-ViewItem--MainImage" src={this.state.image}/>
                     <img className="HUD_Html-ViewItem--Arrow HUD_Html-ViewItem--Arrow-Right" src={arrom_right}/>
                 </div>;
                 
@@ -40,4 +110,4 @@ class HUD_Html extends Component {
     }
 }
 
-export default HUD_Html;
+export default HUD_ViewItem;
