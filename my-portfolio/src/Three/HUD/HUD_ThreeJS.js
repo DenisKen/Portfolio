fe2fb6ceb6307544ea73d37905f7b92693662f4b
DenisKen <denis.kenno@gmail.com>
2019-09-11 18:48:24 -0300

    Update

--- my-portfolio/src/Three/HUD/HUD_ThreeJS.js
+++ my-portfolio/src/Three/HUD/HUD_ThreeJS.js
@@ -7,6 +7,15 @@ class HUD_ThreeJS{
         this.camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 10000 );;
         this.scene = scene;
         this.renderer = renderer;
+
+        this.textureLoader = new THREE.TextureLoader();
+
+        this.spriteMain = null;
+        this.spriteName = null;
+
+        //UP, DOWN, RIGHT, LEFT
+        this.spriteInteractions = [];
+
     }
     
     //Init all sprites objects
@@ -18,19 +27,29 @@ class HUD_ThreeJS{
         //---When in ranged of choice
         //Sprite Background Choice
         //Sprite Options (View, Get, etc)
-
-        var textureLoader = THREE.TextureLoader();
-        textureLoader.load(mouseSelector, (textureLoader) =>{
+        
+        this.textureLoader.load(mouseSelector, (textureLoader) =>{
             var spriteMain_Material = new THREE.SpriteMaterial( { map: textureLoader} );
             spriteMain_Material.transparent = true;
-            var spriteMain = new THREE.Sprite( spriteMain_Material );
+            this.spriteMain = new THREE.Sprite( spriteMain_Material );
+            this.spriteMain.scale.x = 50;
+            this.spriteMain.scale.y = 50;
+            this.spriteMain.scale.z = 50;
+            this.spriteMain.position.z = -200;
+            this.scene.add(this.spriteMain );
+        });
+        
+        this.textureLoader.load(mouseSelector, (textureLoader) =>{
+            var spriteMain_Material = new THREE.SpriteMaterial( { map: textureLoader} );
+            spriteMain_Material.transparent = true;
+            this.spriteMain = new THREE.Sprite( spriteMain_Material );
             spriteMain.scale.x = 50;
             spriteMain.scale.y = 50;
             spriteMain.scale.z = 50;
+            spriteMain.position.y = 50;
             spriteMain.position.z = -200;
             this.scene.add( spriteMain );
         });
-        
     }
 
     loadSprite = (sprite) =>{
