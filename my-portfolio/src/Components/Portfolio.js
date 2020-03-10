8599d04f8083098bf7edd974832aa2612ba958c2
Denis Ken <denis.kenno@gmail.com>
2020-03-10 17:41:14 -0300

    Added Padding to portfolio css

--- my-portfolio/src/Components/Portfolio.js
+++ my-portfolio/src/Components/Portfolio.js
@@ -8,8 +8,6 @@ class Portfolio extends Component {
   constructor(props) {
     super(props);
     this.state = { 
-      width: 0, 
-      height: 0,
       slidePerPage: 1.6 
     };
     this.updateWindowDimensions = this.updateWindowDimensions.bind(this);
@@ -27,9 +25,6 @@ class Portfolio extends Component {
     else 
       this.setState({slidePerPage: 1})
 
-    this.setState({ 
-      width: window.innerWidth, 
-      height: window.innerHeight });
   }  
   
   render() {
@@ -45,37 +40,37 @@ class Portfolio extends Component {
 
               <div className="overlay">
                 <div className="portfolio-item-meta">
-                <h5>TIKI</h5>
-                  <p>
-                    TIKI Game was my first game made in Unity 3D. 
-                    The coolest part of develop it was create the cube's communications.
-                    Each cube communicates with adjancentes, using recursive methods.
-                  </p>
-                  {this.state.width > 932 ? null : null}
-                  <Carousel
-                    className={"portfolio-carousel"}
-                    centered
-                    clickToChange
-                    infinite
-                    
-                    slidesPerPage={this.state.slidePerPage}
-                    arrowLeft={<button class="BrainhubCarousel__arrows BrainhubCarousel__arrowLeft portfolio-carousel-arrows"><span>next</span></button>}
-                    arrowRight={<button class="BrainhubCarousel__arrows BrainhubCarousel__arrowRight portfolio-carousel-arrows portfolio-carousel-arrow-right"><span>next</span></button>}
-                    addArrowClickHandler
-                    >
-                    
-                    <img src={"images/portfolio/tiki_1.png"} />
-                    <img src={"images/portfolio/tiki_2.png"} />
-                    <img src={"images/portfolio/tiki_3.png"} />
-                    <img src={"images/portfolio/tiki_4.png"} />
-                    <img src={"images/portfolio/tiki_5.png"} />
-                    <img src={"images/portfolio/tiki_apng.png"} />
-                  </Carousel>
+                <h5 className="portfolio-text">TIKI</h5>
+                <p  className="portfolio-text">
+                  TIKI Game was my first game made in Unity 3D. 
+                  The coolest part of develop it was create the cube's communications.
+                  Each cube communicates with adjancentes, using recursive methods.
+                </p>
+                {this.state.width > 932 ? null : null}
+                <Carousel
+                  className={"portfolio-carousel"}
+                  centered
+                  clickToChange
+                  infinite
+                  
+                  slidesPerPage={this.state.slidePerPage}
+                  arrowLeft={<button class="BrainhubCarousel__arrows BrainhubCarousel__arrowLeft portfolio-carousel-arrows"><span>next</span></button>}
+                  arrowRight={<button class="BrainhubCarousel__arrows BrainhubCarousel__arrowRight portfolio-carousel-arrows portfolio-carousel-arrow-right"><span>next</span></button>}
+                  addArrowClickHandler
+                  >
+                  
+                  <img src={"images/portfolio/tiki_1.png"} />
+                  <img src={"images/portfolio/tiki_2.png"} />
+                  <img src={"images/portfolio/tiki_3.png"} />
+                  <img src={"images/portfolio/tiki_4.png"} />
+                  <img src={"images/portfolio/tiki_5.png"} />
+                  <img src={"images/portfolio/tiki_apng.png"} />
+                </Carousel>
                 </div>
                 
                 <div className="portfolio-item-meta">
-                  <h5>LISY - A LIPSYNC SOFTWARE</h5>
-                  <p>
+                  <h5 className="portfolio-text">LISY - A LIPSYNC SOFTWARE</h5>
+                  <p className="portfolio-text">
                     LISY is a software of lip synchronization of 3D models. With LISY, 
                     it's possible synchronize speech in real time in any application!
                     Lisy is the winner of Inove Olos award.
@@ -100,8 +95,8 @@ class Portfolio extends Component {
                 </div>
 
                 <div className="portfolio-item-meta">
-                  <h5>VIDEO BOT</h5>
-                  <p>
+                  <h5 className="portfolio-text">VIDEO BOT</h5>
+                  <p className="portfolio-text">
                     Video Bot, also called as OWI is a Web Mobile application to solve any kind of problem.
                     Like: product questions, sale, product presentation, etc.
                   </p>
@@ -125,8 +120,8 @@ class Portfolio extends Component {
                 </div>
 
                 <div className="portfolio-item-meta">
-                  <h5>CONAREC - VR GOOGLE DREAM</h5>
-                  <p>
+                  <h5 className="portfolio-text">CONAREC - VR GOOGLE DREAM</h5>
+                  <p className="portfolio-text">
                     Conarec VR was a project for the most prestigious customer relationship event in Latin America, the CONAREC event.
                     The project was developed in Unity 3D using Google Dream technology. We were one of the first
                     people to import the product from the EUA and apply it in Brazil.
